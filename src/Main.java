c3d757d9549a84b5d743d995b93d5cc0fc8f8993
gunluss <gnluss@hotmail.com>
2026-05-18 19:55:43 +0300

    Added market_data and temporary main system flow

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("=== Algorithmic Trading System Started ===");
+
+        // Singleton: global ayarlar
+        TradingSystemConfig config = TradingSystemConfig.getInstance();
+
+        // file path
+        config.setFilePath("market_data.json");
+
+        // Risk limit
+        config.setRiskLimit(5.0);
+
+        // SMA threshold
+        config.setThresholdValue(0.0);
+
+        // Strategy Type
+        config.setStrategyType("Short-Term");
+
+        System.out.println("File Path: " + config.getFilePath());
+        System.out.println("Risk Limit: " + config.getRiskLimit());
+        System.out.println("Strategy: " + config.getStrategyType());
+
+        System.out.println("\n--- SMA Analysis ---");
+        AnalysisEngine smaEngine = new SMAEngine(3);
+        smaEngine.analyse();
+
+        System.out.println("\n--- ATR Risk Analysis ---");
+        AnalysisEngine atrEngine = new ATREngine(3);
+        atrEngine.analyse();
+
+        System.out.println("\n=== System Finished ===");
     }
 }
