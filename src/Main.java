1f10fe1753253c9f76dd2980efa7f4ac8a1c26bc
bosverimgecim <imgeyuksel427@gmail.com>
2026-05-21 13:48:16 +0300

    Adding  commit names of group member from main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+// BADE BALCI
+// İMGE SÜMBÜL YÜKSEL
+// AHMET FATİH GÜNLÜ
+// BURAK GÜLERYÜZ
+// ALGORITHMIC TRADING SYSTEM
+
+
 public class Main {
     public static void main(String[] args) {
 
